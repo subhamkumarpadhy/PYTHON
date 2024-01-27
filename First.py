09bbeb80629a2c2c418833db7a36409b7ca6c825
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2024-01-27 18:28:21 +0530

    Today is my first day of learning python

---
+++ First.py
@@ -0,0 +1,106 @@
+# print("Hello World")
+# print("Subham is my name.", "My age is 21")
+# print (54+47)
+
+# name = "Subham"
+# age = 21
+# price = 45.99
+# print(name)
+# print("My name is: " , name)
+# print("My age is: " , age)
+# print("My price is: " , price)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# name1 = "kanha"
+# name2 = 'kanha'
+# name3 = '''kanha'''
+# print(name1)
+# print(name2)
+# print(name3)
+
+# age = 23
+# old = False
+# a = None
+# print(type(a))
+# print(type(age))
+# print(type(old))
+
+
+# a = 4
+# b = 8
+# sum =  a + b
+# print(sum)
+
+#power operatorin python
+# a = 5
+# b = 5
+# sum = a ** b
+# print(sum)
+
+
+#logical operators in python
+# a = 50
+# b = 30
+# print(not False)
+# print(not (a > b))
+# val1 = True
+# val2 = False
+# print("AND operator:", val1 and val2)
+# print("OR operator:", val1 or val2)
+# print("OR operator:", (a == b) or (a > b))
+
+
+# Type casting
+# a = 3.14
+# a = str(a)
+# print(type(a))
+# b = float("4")
+# v = 3.65
+# print(b + v)
+
+
+#Taking input
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Welcome", name)
+# print("You entered age is:", age)
+
+#input() by default take input as string
+
+# val = int(input("Enter some value: "))
+# val2 = float(input("Enter some value: "))
+# print(type(val), val)
+# print(type(val2), val2)
+
+
+# name = input("Enter your name ")
+# age = int(input("Enter your age "))
+# marks = float(input("Enter your marks "))
+# print("name = ", name)
+# print("age = ", age)
+# print("marks = ", marks)
+
+
+#some practice problems
+
+#input 2 nums & return sum
+# a = int(input("Enter your 1st number: "))
+# b = int(input("Enter your 2nd number: "))
+# print(a + b)
+
+#input side & return area
+# side = float(input("Enter the side of square: "))
+# print("The area of square is:", side ** 2)
+
+#input 2 float nums & return avg
+# num1 = float(input("Enter first float num: "))
+# num2 = float(input("Enter second float num: "))
+# avg = (num1 + num2) / 2
+# print(avg)
+
+#input 2 int nums & return true or false
+z = int(input("Enter num1: "))
+y = int(input("Enter num2: "))
+print(z >= y)
