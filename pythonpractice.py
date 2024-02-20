47616c28818f5387762f3cc9d0b7fed73d16bb41
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2024-02-20 23:03:16 +0530

    solve some conditional problems.

---
+++ pythonpractice.py
@@ -0,0 +1,65 @@
+# day = input("What day is today:-")
+# age = int(input("Enter your age:"))
+
+# if day == "wednesday":
+#     if age >= 18:
+#         print("Your ticket price is", 10, "$")
+#     else:
+#         print("Your ticket price is", 6, "$")
+# else:
+#     if age >= 18:
+#         print("Your ticket price is", 12, "$")
+#     else:
+#         print("Your ticket price is", 8, "$")
+
+
+# mark = int(input("Enter your mark to know your grade: "))
+# if mark > 100:
+#     print("Marks can't be greater than 100. Please check your marks again.")
+# else:
+#     if mark >= 90:
+#         print("Your grade is A.")
+#     elif mark >= 80:
+#         print("Your grade is B.")
+#     elif mark >= 70:
+#         print("Your grade is C.")
+#     elif mark >= 60:
+#         print("Your grade is D.")
+#     else:
+#         print("Your grade is F.")
+
+
+# fruit = "Banana"
+# colour = input("Enter colour: ")
+# if fruit == "Banana":
+#     if colour == "Green" or colour == "green":
+#         print("It's Unripe.")
+#     elif colour == "Yellow" or colour == "yellow":
+#         print("It's Ripe.")
+#     elif colour == "Brown" or colour == "brown":
+#         print("It's Overripe.")
+
+
+# day = input("What's the day outside? \n")
+# if day == "Sunny":
+#     print("You should go for a walk.")
+# elif day == "Rainy":
+#     print("You should read a book.")
+# elif day == "Snowy":
+#     print("Build a snowman.")
+
+
+# dist = int(input("Enter your distance: "))
+# if dist < 3:
+#     print("You should go by Walk.")
+# elif dist < 15:
+#     print("Take a Bike.")
+# else:
+#     print("Take the Car.")
+
+
+# year = int(input("Enter the year:- \n"))
+# if ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0):
+#     print("It's a leap year.")
+# else:
+#     print("It's not a leap year.")
